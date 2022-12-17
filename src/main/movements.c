314d92a84479032d42dbbaec5c8e22e1ac874d4c
Jessica Goel <jessicagoel@iMac-de-Jessica.local>
2022-12-17 16:50:20 +0100

    east.xpm, few changes to txtr,

--- src/main/movements.c
+++ src/main/movements.c
@@ -127,9 +127,9 @@ void	move_with_key(t_all *all)
 	calculus_view(all);
 	fill_next_pos(all, &all->player.x, &all->player.y);
 	draw_map(all);
-	//drawray(all);
-	//print_wth_is_that(all->player.raycast);
-	//printf("y: %f - x: %f\tangle: %f\n", all->player.y, all->player.x, all->player.angle);
+	drawray(all);
+	print_wth_is_that(all->player.raycast);
+	printf("y: %f - x: %f\tangle: %f\n", all->player.y, all->player.x, all->player.angle);
 }
 
 void	translate_key(int keycode, t_all *all, int value)
